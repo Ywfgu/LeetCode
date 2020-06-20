f255bde6c3a393fe02bfbb40108e12433e1bf4d7
Ywfgu <guhaitao9311@foxmail.com>
2020-06-20 22:21:53 +0800

    auto commit

---
+++ src/test/java/com/leetcode/string/Code3_Substring_LongestWithoutRepeating.java
@@ -0,0 +1,52 @@
+package com.leetcode.string;
+
+import org.junit.Test;
+
+/**
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ *
+ * 示例 1:
+ *
+ * 输入: "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 示例 2:
+ *
+ * 输入: "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 示例 3:
+ *
+ * 输入: "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ */
+public class Code3_Substring_LongestWithoutRepeating {
+
+    public int lengthOfLongestSubstring(String s) {
+        if(s == null || s.length() <= 1 ) return s.length();
+        char[] str = s.toCharArray();
+        int start = 0, maxlenght = 1;
+        for(int i = 1; i < str.length; i++){
+            // 检查str[i] 是否是str[start]到str[end]里已经出现过的字符
+            for(int j = start; j < i; j++){
+                if(str[i] == str[j]){
+                    start = j+1;
+                    break;
+                }
+            }
+            //更新maxLength
+            maxlenght = Math.max(maxlenght, i-start+1);
+        }
+
+        return maxlenght;
+    }
+
+    @Test
+    public void test(){
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(lengthOfLongestSubstring("bbbbb"));
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
+    }
+}
