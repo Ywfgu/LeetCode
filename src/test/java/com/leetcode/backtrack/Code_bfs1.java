64090b7bca7e1f078dca17bca644c34807a34fd3
Ywfgu <guhaitao9311@foxmail.com>
2020-06-27 22:42:55 +0800

    auto commit

---
+++ src/test/java/com/leetcode/backtrack/Code_bfs1.java
@@ -0,0 +1,25 @@
+package com.leetcode.backtrack;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Code_bfs1 {
+
+    @Test
+    public void test(){
+        bfsEnter(new int[]{1,2,3});
+    }
+
+    public List<List<Integer>> bfsEnter(int[] nums){
+        List<List<Integer>> res = new ArrayList<>();
+
+
+        return res;
+    }
+
+    private void bfs(int[] nums, ArrayList<Integer> list, int index, int deep, List<List<Integer>> res) {
+
+    }
+}
